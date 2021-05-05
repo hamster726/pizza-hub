cb17e2fa6a71ad899f5a6c90d3e2382700a365e8
Hamster726 <yevhen.shaparenko@gmail.com>
2021-05-05 16:26:44 +0300

    added selector of type of pizza

--- src/components/CardList/cardItem.js
+++ src/components/CardList/cardItem.js
@@ -4,14 +4,20 @@ import {
   CardContainer,
   CardImage,
   CardTitle,
-  DoughSelector,
-  ImageContainer, QuantityOfPizza,
+  SelectorList,
+  List,
+  InputButton,
+  Label,
+  ImageContainer,
+  QuantityOfPizza,
   SelectorWrapper,
-  SizeSelector,
-  Price
+  Price,
+  PriceContainer,
+  MinusButton,
+  PlusButton,
 } from "./cardStyle";
 
-const CardItem = ({params}) => {
+const CardItem = ({ params }) => {
   return (
     <CardContainer>
       <ImageContainer>
@@ -19,12 +25,58 @@ const CardItem = ({params}) => {
       </ImageContainer>
       <CardTitle>{params.name}</CardTitle>
       <SelectorWrapper>
-        <DoughSelector></DoughSelector>
-        <SizeSelector></SizeSelector>
+        <SelectorList>
+          <List>
+            <InputButton
+              type="radio"
+              id={`slim-dough-${params.key}`}
+              name={`dough-${params.key}`}
+            />
+            <Label for={`slim-dough-${params.key}`}>тонке</Label>
+          </List>
+          <List>
+            <InputButton
+              type="radio"
+              id={`fat-dough-${params.key}`}
+              name={`dough-${params.key}`}
+            />
+            <Label for={`fat-dough-${params.key}`}>традиційне</Label>
+          </List>
+        </SelectorList>
+        <SelectorList>
+          <List>
+            <InputButton
+              type="radio"
+              id={`25sm-size-${params.key}`}
+              name={`size-${params.key}`}
+            />
+            <Label for={`25sm-size-${params.key}`}>25 см</Label>
+          </List>
+          <List>
+            <InputButton
+              type="radio"
+              id={`30sm-size-${params.key}`}
+              name={`size-${params.key}`}
+            />
+            <Label for={`30sm-size-${params.key}`}>30 см</Label>
+          </List>
+          <List>
+            <InputButton
+              type="radio"
+              id={`35sm-size-${params.key}`}
+              name={`size-${params.key}`}
+            />
+            <Label for={`35sm-size-${params.key}`}>35 см</Label>
+          </List>
+        </SelectorList>
       </SelectorWrapper>
-      <Price>{params.pizzaParams.price} грн.</Price>
-      <AddToCartButton></AddToCartButton>
-      <QuantityOfPizza>- 1 +</QuantityOfPizza>
+      <PriceContainer>
+        <Price>{params.pizzaParams.price} грн.</Price>
+        <AddToCartButton>Додати</AddToCartButton>
+        <QuantityOfPizza>
+          <MinusButton /> 1 <PlusButton />
+        </QuantityOfPizza>
+      </PriceContainer>
     </CardContainer>
   );
 };
--- src/components/CardList/cardStyle.js
+++ src/components/CardList/cardStyle.js
@@ -1,11 +1,14 @@
 import styled from "styled-components";
+import PlusIco from "../../img/icons/plus-solid.svg"
+import MinusIco from "../../img/icons/minus-solid.svg"
 
 const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1 1 20%;
-  max-width: 300px;
-  margin: 0 0.5%;
+  max-width: 290px;
+  margin: 3% 0.5%;
+  padding: 10px;
 `;
 
 const ImageContainer = styled.div`
@@ -29,20 +32,149 @@ const CardTitle = styled.h3`
 `;
 
 const SelectorWrapper = styled.div`
-  background: #F3F3F3;
+  background: #f3f3f3;
   border-radius: 10px;
   min-height: 85px;
+  height: 50%;
+  margin-top: 20px;
 `;
 
-const DoughSelector = styled.div``;
+const SelectorList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  margin: 10px;
+  
+  display: flex;
+`;
 
-const SizeSelector = styled.div``;
+const List = styled.li`
 
-const Price = styled.div``;
+  flex: 1 1 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 5px;
+  height: 40px;
+`;
 
-const AddToCartButton = styled.button``;
 
-const QuantityOfPizza = styled.div``;
+const Label = styled.label`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background: rgba(255, 255, 255, 0.3);
+
+  font-weight: bold;
+  font-size: 16px;
+  border-radius: 4px;
+  margin: 0;
+
+  color: #2C2C2C;
+`;
+
+const InputButton = styled.input`
+  opacity: 0;
+  width: 0;
+  &[type="radio"]:checked + ${Label} {
+    background: white;  
+  }
+`;
+
+const PriceContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 17px;
+`;
+
+const Price = styled.div`
+  flex: 1 1 20%;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 22px;
+  line-height: 27px;
+`;
+
+const AddToCartButton = styled.button`
+  //display: block;
+  display: none;
+  position: relative;
+  width: 132px;
+  height: 40px;
+
+  background: #fe5f1e;
+  border: 1px solid #fe5f1e;
+  box-sizing: border-box;
+  border-radius: 30px;
+  padding: 0 20px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 19px;
+  text-align: right;
+  
+  color: #ffffff;
+  
+  &:before {
+    content: "+";
+    position: absolute;
+
+    font-style: normal;
+    font-weight: bold;
+    font-size: 22px;
+    line-height: 19px;
+    top: 50%;
+    left: 20px;
+    
+    transform: translate(-50%,-50%);
+  }
+`;
+
+const QuantityOfPizza = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 132px;
+  height: 40px;
+  border-radius: 30px;
+
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 27px;
+
+`;
+
+const MinusButton = styled.button`
+  display: block;
+  position: relative;
+  padding: 0;
+  
+  border: none;
+  background-color: #fe5f1e;
+  border-radius: 100%;
+  color: white;
+  height: 40px;
+  width: 40px;
+  
+  
+  
+  &:before {
+    position: absolute;
+    content: url(${MinusIco});
+    top:50%;
+    left:50%;
+    width: 80%;
+    transform: translate(-50%, -47%) scale(0.5);
+  }
+
+`;
+const PlusButton = styled(MinusButton)`
+  &:before {
+    content: url(${PlusIco});
+`;
 
 export {
   CardContainer,
@@ -50,9 +182,14 @@ export {
   CardImage,
   CardTitle,
   SelectorWrapper,
-  DoughSelector,
-  SizeSelector,
+  SelectorList,
+  List,
+  InputButton,
+  Label,
   Price,
   AddToCartButton,
   QuantityOfPizza,
+  PriceContainer,
+  MinusButton,
+  PlusButton
 };
