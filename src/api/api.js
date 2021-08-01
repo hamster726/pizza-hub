955a296872b49e0dc6874e0c12170a917e2bbb01
Hamster726 <114generals>
2021-08-02 00:00:30 +0300

    added new api link

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
-
-const _apiBase = "http://localhost:3001"
+const _apiBase = "https://yesha-pizza-hub-default-rtdb.europe-west1.firebasedatabase.app/"
 
 const getCatalogAPI = async () => {
-  return await axios.get(`${_apiBase}/catalog`);
+  return await axios.get(`${_apiBase}/catalog.json`);
 }
 
 export {
